fa2d8960911201a7205b0c7c510b5ea2c91267eb
Freeman86 <freeman86@list.ru>
2020-07-15 17:43:48 +0300

    Update admin.py

--- main_page/admin.py
+++ main_page/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from main_page.models import Main
 
 class MainAdmin(admin.ModelAdmin):
-    list_display = ('nickname', 'date_arrival', 'age', 'weight', 'height', 'special_mark', 'members')
+    list_display = ('nickname', 'date_arrival', 'age', 'weight', 'height', 'special_mark')
     search_fields = ('nickname', 'date_arrival', 'special_mark')
 
 admin.site.register(Main, MainAdmin)
